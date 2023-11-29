29be5778db387e5b5783f666df8bf84f72a502d7
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-29 10:29:42 -0800

    comments and initial

---
+++ strings_methods.js
@@ -0,0 +1,34 @@
+const strings = "strings"
+console.log("Starting Log" + "-" + strings)
+// Length
+for () {
+
+
+}
+
+
+// Slice
+
+
+
+// Replace
+
+
+
+// Replace All
+
+
+
+// Concat
+
+
+
+// CharAt
+
+
+
+// 
+
+
+
+// 
