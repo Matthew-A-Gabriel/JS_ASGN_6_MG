5b3b4222600935457997bb040eab790780fc3930
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-01 10:27:20 -0800

    Length finished

--- strings_methods.js
+++ strings_methods.js
@@ -1,14 +1,28 @@
 const strings = "strings"
-console.log("Starting Log" + "-" + strings)
+console.log("Starting Log" + " - " + strings)
+console.log ("length recreation")
+
 // Length
-for () {
-
-
+function lengthFunction () {
+    let lValue = 0
+    for (var i = 0; i < i+1; i++) {
+        if(strings[i] === undefined){
+            console.log("string ends")
+            return lValue
+            break
+        } else {
+            lValue++
+            console.log(strings[i])
+        }
+    }
+    console.log("length is " + lValue)
 }
+lengthFunction()
 
 
 // Slice
-
+console.log("-Space-")
+console.log("Slice receation")
 
 
 // Replace
