f4b276c2275a4524ac049ccf69e008722a443633
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-12-04 10:27:41 -0800

    Slice done

--- strings_methods.js
+++ strings_methods.js
@@ -3,12 +3,11 @@ console.log("Starting Log" + " - " + strings)
 console.log ("length recreation")
 
 // Length
-function lengthFunction () {
+function lengthFunction() {
     let lValue = 0
     for (var i = 0; i < i+1; i++) {
         if(strings[i] === undefined){
             console.log("string ends")
-            return lValue
             break
         } else {
             lValue++
@@ -20,10 +19,35 @@ function lengthFunction () {
 lengthFunction()
 
 
+
 // Slice
 console.log("-Space-")
-console.log("Slice receation")
+console.log("Slice Receation")
 
+function sliceFunction() {
+    let sInput = 1
+    let eInput = 3
+    let sString = ""
+    for (var i = sInput; i < eInput; i++ ){
+        if(strings[i] === undefined){
+            console.log("string ends")
+            break
+        } else {
+            sString += strings[i]
+        }
+    }
+    console.log("starting at the " + sInput + " letter - " + strings[sInput])
+    console.log("ending at the " + eInput + " letter - " + strings[eInput - 1])
+    if (strings[sInput] === undefined){
+        console.log("the resulting string has a undefined value")
+    } 
+    else if (strings[eInput] === undefined){
+        console.log("the resulting string has a undefined value")}
+    else{
+        console.log("the resulting string is - " + sString)
+    }
+}
+sliceFunction()
 
 // Replace
 
